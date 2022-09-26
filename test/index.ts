5a6266a2dae066d2c127a8e82839af059e970be3
Alex Lakatos <alex.lakatos.qa@gmail.com>
2022-09-26 18:19:55 +0100

    add code coverage for tests

--- test/index.ts
+++ test/index.ts
@@ -4,13 +4,11 @@ import { createContentDigestHeader, DigestAlgorithm, verifyContentDigest} from '
 describe('digest', () => {
     describe('.createContentDigestHeader', () => {
         it('creates a single digest from an empty body (SHA256)', () => {
-
             const test = undefined
             const digest = createContentDigestHeader(test,['sha-256'])
             expect(digest).to.equal('sha-256=:47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=:')
         });
         it('creates a single digest from an empty body (SHA512)', () => {
-
             const test = undefined
             const digest = createContentDigestHeader(test,['sha-512'])
             expect(digest).to.equal('sha-512=:z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg==:')
@@ -39,41 +37,42 @@ describe('digest', () => {
         });
         it('throws  with invalid digest algorithm', () => {
             expect(createContentDigestHeader.bind(undefined,'', ['nonsense' as DigestAlgorithm]))
-            .to.throw(/^Unsupported digest algorithm/)
+                .to.throw(/^Unsupported digest algorithm/)
         });
     });
+    
     describe('.verifyContentDigest', () => {
         it('verifies a single digest (SHA256)', () => {
             expect(verifyContentDigest('{hello:"world"}', 'sha-256=:LsWDvMD3TQ5hD1FciIKL6ePw7YR8BVI5dD6NnJwusRs=:'))
-            .to.be.true
+                .to.be.true
         });
         it('verifies a single digest (SHA512)', () => {
             expect(verifyContentDigest('{hello:"world"}', 'sha-512=:YwRB5Y5G6jIfS1V0gBi59+hVKgu+vFjZKmeXdqMQQjwrwh5hA0vNbwDQi30SCiOK+e2dRs3P4tMo72WT3BfmQg==:'))
-            .to.be.true
+                .to.be.true
         });
         it('doesn\'t verify a single invalid digest (SHA256)', () => {
             expect(verifyContentDigest('{goodbye:"world"}', 'sha-256=:LsWDvMD3TQ5hD1FciIKL6ePw7YR8BVI5dD6NnJwusRs=:'))
-            .to.be.false
+                .to.be.false
         });
         it('doesn\'t verify a single invalid digest (SHA512)', () => {
             expect(verifyContentDigest('{goodbye:"world"}', 'sha-512=:YwRB5Y5G6jIfS1V0gBi59+hVKgu+vFjZKmeXdqMQQjwrwh5hA0vNbwDQi30SCiOK+e2dRs3P4tMo72WT3BfmQg==:'))
-            .to.be.false
+                .to.be.false
         });
         it('throws with invalid digest algorithm', () => {
             expect(verifyContentDigest.bind(undefined, '{hello:"world"}', 'md5=:YwRB5Y5G6jIfS1V0gBi59+hVKgu+vFjZKmeXdqMQQjwrwh5hA0vNbwDQi30SCiOK+e2dRs3P4tMo72WT3BfmQg==:'))
-            .to.throw(/^Unsupported digest algorithm/)
+                .to.throw(/^Unsupported digest algorithm/)
         });
         it('verifies two digests (SHA256 and SHA512)', () => {
             expect(verifyContentDigest('{hello:"world"}', 'sha-256=:LsWDvMD3TQ5hD1FciIKL6ePw7YR8BVI5dD6NnJwusRs=:, sha-512=:YwRB5Y5G6jIfS1V0gBi59+hVKgu+vFjZKmeXdqMQQjwrwh5hA0vNbwDQi30SCiOK+e2dRs3P4tMo72WT3BfmQg==:'))
-            .to.be.true
+                .to.be.true
         });
         it('verifies two digests (SHA256 and SHA512) in any order', () => {
             expect(verifyContentDigest('{hello:"world"}', 'sha-512=:YwRB5Y5G6jIfS1V0gBi59+hVKgu+vFjZKmeXdqMQQjwrwh5hA0vNbwDQi30SCiOK+e2dRs3P4tMo72WT3BfmQg==:, sha-256=:LsWDvMD3TQ5hD1FciIKL6ePw7YR8BVI5dD6NnJwusRs=:'))
-            .to.be.true
+                .to.be.true
         });
-        it('doesn\`t verify if any digest fails', () => {
+        it('doesn\'t verify if any digest fails', () => {
             expect(verifyContentDigest('{hello:"world"}', 'sha-512=:ZwRB5Y5G6jIfS1V0gBi59+hVKgu+vFjZKmeXdqMQQjwrwh5hA0vNbwDQi30SCiOK+e2dRs3P4tMo72WT3BfmQg==:, sha-256=:LsWDvMD3TQ5hD1FciIKL6ePw7YR8BVI5dD6NnJwusRs=:'))
-            .to.be.false
+                .to.be.false
         });
     });
 })
